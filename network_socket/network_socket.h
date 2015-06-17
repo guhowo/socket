ca3086e2c45833f63d5208f69542807e1852b4a3
guhowo <guhowo@gmail.com>
2015-06-18 01:02:45 +0800

    Update network_socket.h

--- network_socket/network_socket.h
+++ network_socket/network_socket.h
@@ -2,7 +2,7 @@
 #define _NETWORK_SOCKET_H_
 
 #define SIN_PORT		8888	/* port number */
-#define LOOPBACK		"127.0.0.1"		/* loopback address */
+#define LOOPBACK		"127.0.0.1"	/* loopback address */
 #define SOCKET_QUEUE_SIZE 	5	/* the queue length */
 
 #endif
